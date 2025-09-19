3f28a1c91ec983db4bc9f1654ced20db5ea6fbbb
Hoàng Minh Đức <hoangminhduc0308@gmail.com>
2025-09-19 17:14:19 +0700

    added template

--- as1/src/main.ts
+++ as1/src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from "vue";
-// import App from "./App.vue";
+// parse event.txt
 
-// createApp(App).mount("#app");
+// import * as fs from "fs";
+// fs.readFileSync("/path-to-file", "utf-8");
+
+class Event {
+  eventid: string = "";
+  eventname: string = "";
+  category: string = "";
+  durationhour: number = 0;
+}
+
+var events: Event[] = [];
+fetch("events.txt")
+  .then((res) => res.json())
+  .then((events) => console.log(events))
+  .catch((err) => console.error(err));
+
+for (let i of events) {
+  console.log(i);
+}
